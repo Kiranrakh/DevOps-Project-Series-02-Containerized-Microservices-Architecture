bc33c1903af98f2d346605e1338c6523f57e86bf
Kiran Barikrao Rakh <124512771+Kiranrakh@users.noreply.github.com>
2025-07-27 08:39:53 +0530

    __init__.py

---
+++ userapp/app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from redis import Redis
+from userapp.config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    try:
+        app.redis = Redis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, decode_responses=True)
+        app.redis.ping()
+        print("✅ Redis connected.")
+    except Exception as e:
+        app.redis = None
+        print(f"⚠️ Redis not available: {e}")
+
+    from .routes import register_routes
+    register_routes(app)
+
+    return app
